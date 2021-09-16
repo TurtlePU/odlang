3ff81554c994cda518f1a94ac7e175a3278028ef
TurtlePU <sokolov.p64@gmail.com>
2021-09-17 00:16:25 +0300

    Result.hs: cleanup

--- src/Result.hs
+++ src/Result.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE DeriveFunctor #-}
 
-module Result (Result (..), Res (..), ok, err, sift) where
+module Result where
 
 import Control.Monad
 import Data.Fix
@@ -21,11 +21,14 @@ instance Semigroup e => Monad (Result e) where
 
 newtype Res e f a = Res {unres :: Result e (f a)} deriving (Functor)
 
+res :: Result e (f a) -> Res e f a
+res value = Res {unres = value}
+
 ok :: f a -> Res e f a
-ok value = Res {unres = Ok value}
+ok = res . Ok
 
 err :: e -> Res e f a
-err value = Res {unres = Err value}
+err = res . Err
 
 sift :: (Semigroup e, Traversable f) => Fix (Res e f) -> Result e (Fix f)
 sift = foldFix $ fmap wrapFix . sequenceA <=< unres

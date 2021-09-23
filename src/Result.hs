28e87e32faad4077dcc58b583a5aa9208c1a15ee
TurtlePU <sokolov.p64@gmail.com>
2021-09-24 01:19:17 +0300

    strip Result

--- src/Result.hs
+++ src/Result.hs
@@ -5,9 +5,8 @@ module Result where
 import Control.Applicative (Alternative (..), Applicative (..))
 import Control.Monad (Monad (..), (>=>))
 import Data.Bifunctor (Bifunctor (..))
-import Data.Fix (Fix (..), foldFix, wrapFix)
 
-data Result e a = Ok a | Err e deriving (Functor)
+data Result e a = Err e | Ok a deriving (Functor)
 
 instance Bifunctor Result where
   bimap f g = result (Err . f) (Ok . g)
@@ -15,36 +14,22 @@ instance Bifunctor Result where
 instance Semigroup e => Applicative (Result e) where
   pure = Ok
 
-  Ok f <*> Ok x = Ok $ f x
-  Ok _ <*> Err e = Err e
-  Err e <*> Ok _ = Err e
   Err l <*> Err r = Err $ l <> r
+  Err e <*> Ok _ = Err e
+  Ok _ <*> Err e = Err e
+  Ok f <*> Ok x = Ok $ f x
 
 instance Semigroup e => Monad (Result e) where
-  Ok x >>= f = f x
   Err e >>= f = Err e
+  Ok x >>= f = f x
 
 instance Monoid e => Alternative (Result e) where
   empty = Err mempty
 
+  Err l <|> Err r = Err $ l <> r
   Ok x <|> _ = Ok x
   _ <|> Ok x = Ok x
-  Err l <|> Err r = Err $ l <> r
 
 result :: (a -> c) -> (b -> c) -> Result a b -> c
 result f _ (Err x) = f x
 result _ f (Ok x) = f x
-
-newtype Res e f a = Res {unres :: Result e (f a)} deriving (Functor)
-
-res :: Result e (f a) -> Res e f a
-res value = Res {unres = value}
-
-ok :: f a -> Res e f a
-ok = res . Ok
-
-err :: e -> Res e f a
-err = res . Err
-
-sift :: (Semigroup e, Traversable f) => Fix (Res e f) -> Result e (Fix f)
-sift = foldFix $ unres >=> fmap wrapFix . sequenceA

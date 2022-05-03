ff87a725d01ca3c9b848204fd5573c7a7bfe21fe
TurtlePU <sokolov.p64@gmail.com>
2022-05-03 21:22:38 +0300

    collections stuff

--- src/HashMultiMap.hs
+++ src/HashMultiMap.hs
@@ -1,9 +1,19 @@
 module HashMultiMap where
 
-import Data.HashMap.Strict (HashMap, singleton, unionWith)
-import Data.Hashable (Hashable)
+import Data.HashMap.Strict (HashMap)
+import qualified Data.HashMap.Strict as HashMap
+import Data.Hashable
 
 newtype HashMultiMap k v = Multi {unMulti :: HashMap k v}
 
+empty :: HashMultiMap k v
+empty = Multi HashMap.empty
+
+singleton :: (Eq k, Hashable k) => k -> v -> HashMultiMap k v
+singleton k v = Multi (HashMap.singleton k v)
+
 instance (Eq k, Hashable k, Semigroup v) => Semigroup (HashMultiMap k v) where
-  l <> r = Multi $ unionWith (<>) (unMulti l) (unMulti r)
+  l <> r = Multi $ HashMap.unionWith (<>) (unMulti l) (unMulti r)
+
+instance (Eq k, Hashable k, Semigroup v) => Monoid (HashMultiMap k v) where
+  mempty = Multi mempty

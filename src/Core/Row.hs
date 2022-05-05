3d395dc1d6b0690a3109ca775e4f03023cdfd020
TurtlePU <sokolov.p64@gmail.com>
2022-05-05 15:11:04 +0300

    + row equality

--- src/Core/Row.hs
+++ src/Core/Row.hs
@@ -2,33 +2,16 @@
 
 module Core.Row where
 
-import Core.Kind
+import Core.Kind (ProperKind (Row))
 import Data.Bifoldable (Bifoldable (..))
-import Data.Hashable (Hashable)
-import Data.List.NonEmpty (NonEmpty)
-import Data.MultiSet (MultiSet)
-import qualified Data.MultiSet as MultiSet
-import HashMultiMap (HashMultiMap)
+import Data.HashMap.Strict (HashMap)
+import qualified Data.HashMap.Strict as HashMap
+import Data.List.NonEmpty (NonEmpty (..))
+import qualified Data.List.NonEmpty as NonEmpty
+import HashMultiMap (HashMultiMap (..))
 import qualified HashMultiMap
 
-data Row k v r = MkRow
-  { rowLit :: HashMultiMap k [v],
-    rowVar :: MultiSet r
-  }
-
-fromList :: (Eq k, Hashable k) => [(k, v)] -> Row k v r
-fromList =
-  flip MkRow MultiSet.empty
-    . foldMap (\(k, v) -> HashMultiMap.singleton k [v])
-
-fromVar :: Ord r => r -> Row k v r
-fromVar = MkRow HashMultiMap.empty . MultiSet.singleton
-
-instance (Eq k, Hashable k, Ord r) => Semigroup (Row k v r) where
-  MkRow l v <> MkRow l' v' = MkRow (l <> l') (v <> v')
-
-instance (Eq k, Hashable k, Ord r) => Monoid (Row k v r) where
-  mempty = MkRow mempty mempty
+type EntryKey = String
 
 data RowT l a
   = RLit l
@@ -44,7 +27,7 @@ instance Bifoldable RowT where
 
 data RowLit k t
   = REmpty k
-  | REntry String t
+  | REntry EntryKey t
   deriving (Show)
 
 instance Bifoldable RowLit where
@@ -54,7 +37,58 @@ instance Bifoldable RowLit where
 
 type RowTerm t = RowT (RowLit ProperKind t) t
 
-collect :: Applicative f => (t -> f ProperKind) -> RowTerm t -> f [ProperKind]
-collect g = sequenceA . bifoldMap (pure . fmap Row . visit) (pure . g)
+collectK :: Applicative f => (t -> f ProperKind) -> RowTerm t -> f [ProperKind]
+collectK g = sequenceA . bifoldMap (pure . fmap Row . visit) (pure . g)
   where
     visit = bifoldr (const . pure) (const . g) undefined
+
+checkRowEQ :: (t -> t -> Bool) -> RowTerm t -> RowTerm t -> [RowEqError]
+checkRowEQ f l r =
+  let MkRow (Multi ll) lv = intoRow l
+      MkRow (Multi rl) rv = intoRow r
+      eqs = HashMap.intersectionWith checkBagEQ' ll rl
+   in [EKeys | HashMap.keysSet ll /= HashMap.keysSet rl]
+        ++ HashMap.foldMapWithKey mkUnder eqs
+        ++ [EVars | not (checkBagEQ f lv rv)]
+  where
+    intoRow = bifoldMap fromLit fromVar
+    checkBagEQ' l r = checkBagEQ f (NonEmpty.toList l) (NonEmpty.toList r)
+    fromLit = \case
+      REmpty _ -> mempty
+      REntry k v -> fromEntry k v
+    mkUnder l = \case
+      True -> [EUnder l]
+      False -> []
+
+checkBagEQ :: (t -> t -> Bool) -> [t] -> [t] -> Bool
+checkBagEQ eq l r = checkCompsEq (intoComps l) (intoComps r)
+  where
+    intoComps = foldr compInsert []
+    compInsert x = \case
+      [] -> [x :| []]
+      (y : ys) ->
+        if x `eq` NonEmpty.head y
+          then NonEmpty.cons x y : ys
+          else y : compInsert x ys
+    checkCompsEq l r = length l == length r && all (flip any l . compEq) r
+    compEq xs ys =
+      length xs == length ys && NonEmpty.head xs `eq` NonEmpty.head ys
+
+data Row t r = MkRow
+  { rowLit :: HashMultiMap EntryKey (NonEmpty t),
+    rowVar :: [r]
+  }
+
+fromEntry :: EntryKey -> t -> Row t r
+fromEntry k v = MkRow (HashMultiMap.singleton k (v :| [])) []
+
+fromVar :: r -> Row t r
+fromVar x = MkRow HashMultiMap.empty [x]
+
+instance Semigroup (Row t r) where
+  MkRow l v <> MkRow l' v' = MkRow (l <> l') (v <> v')
+
+instance Monoid (Row t r) where
+  mempty = MkRow mempty mempty
+
+data RowEqError = EVars | EKeys | EUnder EntryKey

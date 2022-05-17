183e2a61d55a4e73380aa46c482ab888a6e8269f
TurtlePU <sokolov.p64@gmail.com>
2022-05-17 04:48:42 +0300

    remove Positioned from the face of the Earth

--- src/Core/Term.hs
+++ src/Core/Term.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE LambdaCase #-}
+
 module Core.Term where
 
 import Control.Arrow ((>>>))
@@ -7,6 +9,7 @@ import Core.Type.Syntax (ProperKind)
 import qualified Core.Type.Syntax as TL
 import Data.Bifunctor
 import Data.Either (partitionEithers)
+import Data.Fix (Fix)
 import Data.HashMap.Strict (alter, elems)
 import Data.IndexedBag
 import Data.List.NonEmpty (NonEmpty (..))
@@ -66,45 +69,46 @@ type TypingErrors = NonEmpty TypingError
 
 type TypingResult = CtxResult ([ProperKind], [Type]) TypingErrors
 
-synthesizeType :: Positioned Term -> TypingResult Type
-synthesizeType (Posed _ TVar) = CtxR $ \(_, [ty]) -> pure ty
-synthesizeType (Posed p (TAbs n ty tm mul)) = do
-  isType ty
-  getCtx >>= checkSup' mul
-  checkSup' (upperBound' p n) [ty]
-  Posed p . KPretype . PArrow ty
-    <$> modifyCtx (replicate n ty ++) (synthesizeType tm)
-synthesizeType (Posed _ (TApp s f x)) = do
-  (tyf, tyx) <- splitCtx s (synthesizeType f) $ pairM (synthesizeType x)
-  (tyarg, tyres) <- pullArrow tyf
-  checkTyEq tyarg tyx
-  pure tyres
-synthesizeType (Posed p (TGen k tm mul)) = do
-  getCtx >>= checkSup' mul
-  Posed p . KPretype . PForall k <$> shiftCtx k (synthesizeType tm)
-  where
-    shiftCtx k = mapCtx $ bimap (k :) $ fmap $ flip shiftTLTerm 1
-synthesizeType (Posed p (TInst tm arg)) = do
-  (k, ty) <- synthesizeType tm >>= pullForall
-  fromKinding $ checkKind k arg
-  pure $ substTLTerm ty 0 arg
-synthesizeType (Posed p (TAndI s rs mul)) = do
-  tys <- withValues (splitCtxN s . fmap synthesizeType) rs
-  checkSup' mul tys
-  pure $ mkAlgebraic CAnd p tys
-synthesizeType (Posed p (TAndE s tup order tm)) =
-  splitCtx s (synthesizeType tup) $ \tytup -> do
-    addend <- reorder tytup order
-    modifyCtx (addend ++) (synthesizeType tm)
-synthesizeType (Posed p (TWithI rs mul)) = do
-  tys <- traverse synthesizeType rs
-  checkSup' mul tys
-  pure $ mkAlgebraic CWith p tys
-synthesizeType (Posed p (TWithE tm key)) = do
-  ty <- synthesizeType tm
-  pickTy key ty
-synthesizeType (Posed p (TOrI key tm row mul)) = _
-synthesizeType (Posed p (TOrE s tm rs)) = _
+synthesizeType :: Fix Term -> TypingResult Type
+synthesizeType = foldFix $ \case
+  TVar -> CtxR $ \(_, [ty]) -> pure ty
+  TAbs n ty tm mul -> do
+    isType ty
+    getCtx >>= checkSup' mul
+    checkSup' (upperBound' p n) [ty]
+    TData _ . Join . PArrow ty
+      <$> modifyCtx (replicate n ty ++) (synthesizeType tm)
+  TApp s f x -> do
+    (tyf, tyx) <- splitCtx s (synthesizeType f) $ pairM (synthesizeType x)
+    (tyarg, tyres) <- pullArrow tyf
+    checkTyEq tyarg tyx
+    pure tyres
+  TGen k tm mul -> do
+    getCtx >>= checkSup' mul
+    Posed p . KPretype . PForall k <$> shiftCtx k (synthesizeType tm)
+    where
+      shiftCtx k = mapCtx $ bimap (k :) $ fmap $ flip shiftTLTerm 1
+  TInst tm arg -> do
+    (k, ty) <- synthesizeType tm >>= pullForall
+    fromKinding $ checkKind k arg
+    pure $ substTLTerm ty 0 arg
+  TAndI s rs mul -> do
+    tys <- withValues (splitCtxN s . fmap synthesizeType) rs
+    checkSup' mul tys
+    pure $ mkAlgebraic CAnd p tys
+  TAndE s tup order tm ->
+    splitCtx s (synthesizeType tup) $ \tytup -> do
+      addend <- reorder tytup order
+      modifyCtx (addend ++) (synthesizeType tm)
+  TWithI rs mul -> do
+    tys <- traverse synthesizeType rs
+    checkSup' mul tys
+    pure $ mkAlgebraic CWith p tys
+  TWithE tm key -> do
+    ty <- synthesizeType tm
+    pickTy key ty
+  TOrI key tm row mul -> _
+  TOrE s tm rs -> _
 
 pairM :: Monad m => m b -> a -> m (a, b)
 pairM b = flip fmap b . (,)
--- src/Data/Position.hs
+++ src/Data/Position.hs
@@ -1,6 +1,5 @@
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE EmptyCase #-}
-{-# LANGUAGE TupleSections #-}
 
 module Data.Position where
 
@@ -23,24 +22,3 @@ instance Semigroup Position where
 
 instance Monoid Position where
   mempty = undefined
-
-data Positioned f = Posed
-  { pos :: Position,
-    rec :: f (Positioned f)
-  }
-
-withPosition ::
-  (f (Positioned f) -> CtxResult (s, Position) e a) ->
-  Positioned f ->
-  CtxResult s e a
-withPosition g (Posed p r) = mapCtx (,p) (g r)
-
-getPosition :: CtxResult (s, Position) e Position
-getPosition = CtxR $ \(_, p) -> Ok p
-
-foldPositioned ::
-  Functor f =>
-  (f (CtxResult (s, Position) e a) -> CtxResult (s, Position) e a) ->
-  f (Positioned f) ->
-  CtxResult (s, Position) e a
-foldPositioned g = g . fmap (mapCtx fst . withPosition (foldPositioned g))

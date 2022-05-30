3ea0e5adfe50d250eed51ab28b94110a83656dd2
TurtlePU <sokolov.p64@gmail.com>
2022-05-30 11:56:29 +0300

    + simple substitution test

--- test/Core.hs
+++ test/Core.hs
@@ -2,6 +2,7 @@ module Core (core) where
 
 import Control.Monad.Free (Free (..))
 import Control.Monad.FreeBi (FreeBi (..))
+import Core.Type.Evaluation (Substitution (..), substitute)
 import Core.Type.Kinding (synthesizeKind)
 import Core.Type.Syntax
 import Data.Aps (Ap2 (..))
@@ -13,28 +14,51 @@ import Test.Tasty (TestTree, testGroup)
 import Test.Tasty.HUnit (testCase)
 
 core :: TestTree
-core = testGroup "Core" [kinding]
+core = testGroup "Core" [kinding, typeSubstitution]
 
 kinding :: TestTree
 kinding =
   testGroup
     "Kinding"
-    [ testCase "Identity" $ synth (abs Type $ var 0) @?= Ok (Type :->: Type),
-      testCase "Unit" $ synth (record $ rem Type) @?= Ok (Simple Data),
+    [ testCase "Identity" $ synth (labs Type $ var 0) @?= Ok (Type :->: Type),
+      testCase "Unit" $ synth (record $ remp Type) @?= Ok (Simple Data),
       testCase "First" $
-        synth (fst $ pair (mlit (False, True), rem (Simple Data))) @?= Ok Mult
+        synth (lfst $ pair (mlit (False, True), remp (Simple Data))) @?= Ok Mult
     ]
   where
     synth = runCtx [] . synthesizeKind
-    lam = Fix . TLam
-    var = lam . LVar
-    abs k = lam . LAbs k
-    fst = lam . LFst mempty
-    pair = lam . uncurry LPair
-    dat = Fix . TData mempty
-    spread c = dat . PSpread c
-    record = spread CAnd
-    row = Fix . TRow mempty . Join . FreeBi . Free . Ap2
-    rem = row . REmpty
-    mul = Fix . TMul mempty
-    mlit = mul . FreeBi . Free . Ap2 . MLit . uncurry MultLit
+
+typeSubstitution :: TestTree
+typeSubstitution =
+  testGroup
+    "Type substitution"
+    [ testCase "Simple" $
+        substitute (SubWith (var 1)) (pair (var 1, var (-1)))
+          @?= pair (var 0, var (-1))
+    ]
+
+----------------------------- CONSTRUCTOR HELPERS ------------------------------
+
+lam = Fix . TLam
+
+var = lam . LVar
+
+labs k = lam . LAbs k
+
+lfst = lam . LFst mempty
+
+pair = lam . uncurry LPair
+
+dat = Fix . TData mempty
+
+spread c = dat . PSpread c
+
+record = spread CAnd
+
+row = Fix . TRow mempty . Join . FreeBi . Free . Ap2
+
+remp = row . REmpty
+
+mul = Fix . TMul mempty
+
+mlit = mul . FreeBi . Free . Ap2 . MLit . uncurry MultLit

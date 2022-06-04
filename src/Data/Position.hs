49683484ab8d5b21dd4171df66e9fbbf9d2eab56
TurtlePU <sokolov.p64@gmail.com>
2022-06-04 15:31:16 +0300

    trivialize Position equality

--- src/Data/Position.hs
+++ src/Data/Position.hs
@@ -1,17 +1,18 @@
-{-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 
 module Data.Position where
 
-import Data.Hashable (Hashable)
-import GHC.Generics (Generic)
+import Data.Hashable (Hashable (..))
 
-data PositionEntry = PEntry {line :: Int, column :: Int}
-  deriving (Generic, Eq, Show)
-
-instance Hashable PositionEntry
+data PositionEntry = PEntry {line :: Int, column :: Int} deriving (Show)
 
 newtype Position = Pos [PositionEntry]
-  deriving newtype (Hashable, Semigroup, Monoid)
-  deriving (Eq, Show)
+  deriving newtype (Semigroup, Monoid)
+  deriving (Show)
+
+instance Eq Position where
+  _ == _ = True
+
+instance Hashable Position where
+  hashWithSalt = const
